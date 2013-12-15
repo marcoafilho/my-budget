27dff2e59e08eb308f0b702c1e36656aa9f72092
Marco Antonio Almeida <marco.almeida@karnovgroup.com>
2013-12-15 17:26:27 +0100

    Defines user model
    [#57301716]

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_secure_password validations: false
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
+end
